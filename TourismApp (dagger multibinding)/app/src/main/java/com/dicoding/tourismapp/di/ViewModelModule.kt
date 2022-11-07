90f3e136e4fa8b7312742caaed90e614cc818251
fachridantm <fachridantm@gmail.com>
2022-11-07 20:34:55 +0700

    finish codelab dagger multibinding

---
+++ TourismApp (dagger multibinding)/app/src/main/java/com/dicoding/tourismapp/di/ViewModelModule.kt
@@ -0,0 +1,32 @@
+package com.dicoding.tourismapp.di
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.tourismapp.core.ui.ViewModelFactory
+import com.dicoding.tourismapp.detail.DetailTourismViewModel
+import com.dicoding.tourismapp.favorite.FavoriteViewModel
+import com.dicoding.tourismapp.home.HomeViewModel
+import dagger.Binds
+import dagger.Module
+import dagger.multibindings.IntoMap
+
+@Module
+abstract class ViewModelModule {
+    @Binds
+    @IntoMap
+    @ViewModelKey(HomeViewModel::class)
+    abstract fun bindHomeViewModel(homeViewModel: HomeViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(FavoriteViewModel::class)
+    abstract fun bindFavoriteViewModel(favoriteViewModel: FavoriteViewModel): ViewModel
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(DetailTourismViewModel::class)
+    abstract fun bindDetailTourismViewModel(viewModel: DetailTourismViewModel): ViewModel
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+}
