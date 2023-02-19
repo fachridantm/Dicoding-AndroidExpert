9ea487ab8765bb81c7c2dffd5a57526995258e8d
fachridantm <fachridantm@gmail.com>
2023-02-19 16:16:31 +0700

    change follow domain into user

--- GithubApp/core/src/main/java/com/dicoding/githubapp/core/domain/repository/IUserRepository.kt
+++ GithubApp/core/src/main/java/com/dicoding/githubapp/core/domain/repository/IUserRepository.kt
@@ -1,19 +1,18 @@
 package com.dicoding.githubapp.core.domain.repository
 
 import com.dicoding.githubapp.core.data.source.Resource
-import com.dicoding.githubapp.core.data.source.local.entity.UserEntity
-import com.dicoding.githubapp.core.domain.model.Follow
 import com.dicoding.githubapp.core.domain.model.User
 import kotlinx.coroutines.flow.Flow
 
 interface IUserRepository {
     fun searchUser(query: String): Flow<Resource<List<User>>>
     fun getDetailUser(username: String?): Flow<Resource<User>>
-    fun getUserFollowers(username: String): Flow<Resource<List<Follow>>>
-    fun getUserFollowing(username: String): Flow<Resource<List<Follow>>>
-    fun getFavoritedUsers(): Flow<Resource<List<UserEntity>>>
-    suspend fun insertUser(user: UserEntity)
-    suspend fun deleteUser(user: UserEntity)
+    fun getUserFollowers(username: String): Flow<Resource<List<User>>>
+    fun getUserFollowing(username: String): Flow<Resource<List<User>>>
+    fun getFavoritedUsers(): Flow<List<User>>
+
+    suspend fun setFavoriteUsers(user: User, state: Boolean)
+
     fun getThemeSetting(): Flow<Boolean>
     suspend fun setThemeSetting(newSetting: Boolean)
 }
