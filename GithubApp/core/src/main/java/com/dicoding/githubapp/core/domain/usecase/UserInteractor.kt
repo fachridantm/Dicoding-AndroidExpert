aee947643f21a6fb1156150b7c48f8d66b1095d7
fachridantm <fachridantm@gmail.com>
2023-02-19 16:17:21 +0700

    change name to UserInteractor.kt

--- GithubApp/core/src/main/java/com/dicoding/githubapp/core/domain/usecase/UserInteractor.kt
+++ GithubApp/core/src/main/java/com/dicoding/githubapp/core/domain/usecase/UserInteractor.kt
@@ -1,14 +1,12 @@
 package com.dicoding.githubapp.core.domain.usecase
 
 import com.dicoding.githubapp.core.data.source.Resource
-import com.dicoding.githubapp.core.data.source.local.entity.UserEntity
-import com.dicoding.githubapp.core.domain.model.Follow
 import com.dicoding.githubapp.core.domain.model.User
 import com.dicoding.githubapp.core.domain.repository.IUserRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class UserUseCaseImpl @Inject constructor(
+class UserInteractor @Inject constructor(
     private val userRepository: IUserRepository,
 ) : UserUseCase {
 
@@ -18,18 +16,15 @@ class UserUseCaseImpl @Inject constructor(
     override fun getDetailUser(username: String?): Flow<Resource<User>> =
         userRepository.getDetailUser(username)
 
-    override fun getUserFollowers(username: String): Flow<Resource<List<Follow>>> =
+    override fun getUserFollowers(username: String): Flow<Resource<List<User>>> =
         userRepository.getUserFollowers(username)
 
-    override fun getUserFollowing(username: String): Flow<Resource<List<Follow>>> =
+    override fun getUserFollowing(username: String): Flow<Resource<List<User>>> =
         userRepository.getUserFollowing(username)
 
-    override fun getFavoritedUsers(): Flow<Resource<List<UserEntity>>> =
-        userRepository.getFavoritedUsers()
-
-    override suspend fun insertUser(user: UserEntity) = userRepository.insertUser(user)
-
-    override suspend fun deleteUser(user: UserEntity) = userRepository.deleteUser(user)
+    override fun getFavoritedUsers(): Flow<List<User>> = userRepository.getFavoritedUsers()
+    override suspend fun setFavoriteUsers(user: User, state: Boolean) =
+        userRepository.setFavoriteUsers(user, state)
 
     override fun getThemeSetting(): Flow<Boolean> = userRepository.getThemeSetting()
 
