0b8698d8a6493e6f283efc2575befd40b48264d1
fachridantm <fachridantm@gmail.com>
2023-02-19 15:56:59 +0700

    merge follow domain into user

--- GithubApp/core/src/main/java/com/dicoding/githubapp/core/domain/model/Follow.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.githubapp.core.domain.model
-
-data class Follow(
-    val username: String,
-    val avatar: String,
-    val url: String,
-    val followingUrl: String,
-    val followersUrl: String,
-)
