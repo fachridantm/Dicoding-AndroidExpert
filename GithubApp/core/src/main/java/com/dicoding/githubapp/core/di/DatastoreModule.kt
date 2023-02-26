df650e6de8757862b4c0d0158b52dc026dffcf91
fachridantm <fachridantm@gmail.com>
2023-02-26 22:26:40 +0700

    rename file

--- GithubApp/core/src/main/java/com/dicoding/githubapp/core/di/DatastoreModule.kt
+++ GithubApp/core/src/main/java/com/dicoding/githubapp/core/di/DatastoreModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class DataStoreModule {
+class DatastoreModule {
     @Provides
     @Singleton
     fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
