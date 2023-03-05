e7369eeae9fdcb7fb6f4ea56066fa96a232e1495
fachridantm <fachridantm@gmail.com>
2023-03-05 21:48:58 +0700

    init commit

---
+++ MyLeakyApp/app/src/main/java/com/dicoding/myleakyapp/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.dicoding.myleakyapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var broadcastReceiver: BroadcastReceiver
+    private lateinit var tvPowerStatus: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tvPowerStatus = findViewById(R.id.tv_power_status)
+    }
+
+    private fun registerBroadcastReceiver() {
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    Intent.ACTION_POWER_CONNECTED -> tvPowerStatus.text =
+                        getString(R.string.power_connected)
+                    Intent.ACTION_POWER_DISCONNECTED -> tvPowerStatus.text =
+                        getString(R.string.power_disconnected)
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+        registerReceiver(broadcastReceiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadcastReceiver()
+    }
+
+    // Don't forget to unregister the broadcast receiver onStop lifecycle method
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(broadcastReceiver)
+    }
+}
