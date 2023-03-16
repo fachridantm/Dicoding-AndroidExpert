3b04ee5ef0183bdfa233814fc5c4c2cb63c44f54
fachridantm <fachridantm@gmail.com>
2023-03-16 22:43:17 +0700

    encrypt file

--- MySimpleLogin (encryption)/app/src/main/java/com/dicoding/mysimplelogin/SessionManager.kt
+++ MySimpleLogin (encryption)/app/src/main/java/com/dicoding/mysimplelogin/SessionManager.kt
@@ -2,6 +2,12 @@ package com.dicoding.mysimplelogin
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.os.Build
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.securepreferences.SecurePreferences
 
 class SessionManager(context: Context) {
     companion object {
@@ -9,7 +15,33 @@ class SessionManager(context: Context) {
         const val KEY_USERNAME = "username"
     }
 
-    private var pref: SharedPreferences = context.getSharedPreferences("Session", Context.MODE_PRIVATE)
+    private var pref: SharedPreferences = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val spec = KeyGenParameterSpec.Builder(
+            MasterKey.DEFAULT_MASTER_KEY_ALIAS,
+            KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+        )
+
+            .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+            .setKeySize(MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE)
+            .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+            .build()
+
+        val masterKey = MasterKey.Builder(context)
+            .setKeyGenParameterSpec(spec)
+            .build()
+
+        EncryptedSharedPreferences.create(
+            context,
+            "Session",
+            masterKey,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+
+    } else {
+        SecurePreferences(context, "dicoding", "Session")
+    }
+
     private var editor: SharedPreferences.Editor = pref.edit()
 
     fun createLoginSession() {
