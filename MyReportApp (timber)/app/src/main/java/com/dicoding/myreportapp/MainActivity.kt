7f6e4579c5f59ebac174bfcc1a3636e555e9e0b5
fachridantm <fachridantm@gmail.com>
2023-03-18 23:29:04 +0700

    plant timber to MainActivity.kt

--- MyReportApp (timber)/app/src/main/java/com/dicoding/myreportapp/MainActivity.kt
+++ MyReportApp (timber)/app/src/main/java/com/dicoding/myreportapp/MainActivity.kt
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.google.firebase.crashlytics.FirebaseCrashlytics
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Timber.tag("MainActivity: ").d("Test debug log")
+
         val btnCrash = findViewById<Button>(R.id.button)
         btnCrash.setOnClickListener {
             FirebaseCrashlytics.getInstance().log("Clicked on button")
             FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
-            throw RuntimeException("This is a test crash")
+            try {
+                throw RuntimeException("This is a test crash")
+            } catch (e: Exception) {
+                Timber.e("Test non fatal exception")
+            }
         }
     }
 }
