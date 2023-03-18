611d9e4930e080c9700e9fdf78fe9b1c2dd80589
fachridantm <fachridantm@gmail.com>
2023-03-18 22:22:03 +0700

    add firebase crashlytics

--- MyReportApp/app/src/main/java/com/dicoding/myreportapp/MainActivity.kt
+++ MyReportApp/app/src/main/java/com/dicoding/myreportapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.dicoding.myreportapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCrash = findViewById<Button>(R.id.button)
+        btnCrash.setOnClickListener {
+            FirebaseCrashlytics.getInstance().log("Clicked on button")
+            FirebaseCrashlytics.getInstance().setCustomKey("str_key", "some_data")
+            throw RuntimeException("This is a test crash")
+        }
     }
 }
